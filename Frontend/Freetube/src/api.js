bff60a86a44680ae5fc462f1297406faac12d0f9
ACE <satyayadav@satyas-MacBook-Pro.local>
2025-11-18 00:32:46 +0530

    backend link added

--- Frontend/Freetube/src/api.js
+++ Frontend/Freetube/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000/';
+const BASE_URL = 'https://freetube-dvts.onrender.com/';
 
 export async function fetchUsers(token) {
   const res = await fetch(`${BASE_URL}/users`, {
